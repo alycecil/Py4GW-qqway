06dc96ab3b97b7a630f365816a5ac1f6757d7d00
Alice <alycecil@gmail.com>
2026-03-09 21:41:48 -0400

    reenable auto skill

--- Sources/oazix/CustomBehaviors/skills/paragon/save_yourselves_utility.py
+++ Sources/oazix/CustomBehaviors/skills/paragon/save_yourselves_utility.py
@@ -59,3 +59,31 @@ class SaveYourselfsUtility(ProtectiveShoutUtility):
         result:BehaviorResult = yield from custom_behavior_helpers.Actions.cast_skill(self.custom_skill)
         CustomBehaviorParty().get_shared_lock_manager().release_lock(lock_key)
         return result
+
+class SaveYourSelfKurzUtility(SaveYourselfsUtility):
+    def __init__(
+            self,
+            event_bus: EventBus,
+            current_build: list[CustomSkill],
+            score_definition: ScoreStaticDefinition = ScoreStaticDefinition(90)
+    ) -> None:
+
+        super().__init__(
+            event_bus=event_bus,
+            skill=CustomSkill("Save_Yourselves_kurzick"),
+            current_build=current_build,
+            score_definition=score_definition)
+
+class SaveYourSelfLuxonUtility(SaveYourselfsUtility):
+    def __init__(
+            self,
+            event_bus: EventBus,
+            current_build: list[CustomSkill],
+            score_definition: ScoreStaticDefinition = ScoreStaticDefinition(90)
+    ) -> None:
+
+        super().__init__(
+            event_bus=event_bus,
+            skill=CustomSkill("Save_Yourselves_luxon"),
+            current_build=current_build,
+            score_definition=score_definition)
