b020a12d2bdda83c7c170dbf6d7f0b845dcaf6b4
Alice <alycecil@gmail.com>
2026-03-14 15:33:44 -0400

    rupture destruction

---
+++ Sources/oazix/CustomBehaviors/skills/ritualist/rupture_soul_utility.py
@@ -0,0 +1,55 @@
+from typing import Any, Generator, override
+
+from Py4GWCoreLib import Routines, Range
+from Py4GWCoreLib.enums import SpiritModelID
+from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
+from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
+from Sources.oazix.CustomBehaviors.primitives.helpers import custom_behavior_helpers
+from Sources.oazix.CustomBehaviors.primitives.helpers.behavior_result import BehaviorResult
+from Sources.oazix.CustomBehaviors.primitives.scores.score_static_definition import ScoreStaticDefinition
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill import CustomSkill
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
+
+class RuptureSoulUtility(CustomSkillUtilityBase):
+    def __init__(self,
+        event_bus: EventBus,
+        current_build: list[CustomSkill],
+        score_definition: ScoreStaticDefinition = ScoreStaticDefinition(20),
+        mana_required_to_cast: int = 0,
+        allowed_states: list[BehaviorState] = [BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO]
+        ) -> None:
+
+        super().__init__(
+            event_bus=event_bus,
+            skill=CustomSkill("Rupture_Soul"),
+            in_game_build=current_build,
+            score_definition=score_definition,
+            mana_required_to_cast=mana_required_to_cast,
+            allowed_states=allowed_states)
+                
+        self.score_definition: ScoreStaticDefinition = score_definition
+
+        # todo propose to be customizable
+        self.target_spirit_skill: CustomSkill = CustomSkill("Destruction")
+        self.target_spirit_spirit_model_id: SpiritModelID = SpiritModelID.DESTRUCTION
+
+    @override
+    def _evaluate(self, current_state: BehaviorState, previously_attempted_skills: list[CustomSkill]) -> float | None:
+
+        if not Routines.Checks.Skills.IsSkillIDReady(self.target_spirit_skill.skill_id): return None # if target_spirit not-ready, we can't cast
+
+        is_target_spirit_spirit_exist = custom_behavior_helpers.Targets.get_first_or_default_from_spirits_raw(within_range=Range.Spirit, spirit_model_ids=[self.target_spirit_spirit_model_id], condition=lambda agent_id: True)
+        if not is_target_spirit_spirit_exist: return None # no cast, if target_spirit spirit not exist
+
+        return self.score_definition.get_score()
+
+    @override
+    def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
+
+        # we target Destruction spirit to destroy it
+
+        target_spirit_spirit: custom_behavior_helpers.SpiritAgentData | None = custom_behavior_helpers.Targets.get_first_or_default_from_spirits_raw(within_range=Range.Spirit, spirit_model_ids=[self.target_spirit_spirit_model_id], condition=lambda agent_id: True)
+        if target_spirit_spirit is None: return BehaviorResult.ACTION_SKIPPED
+
+        result = yield from custom_behavior_helpers.Actions.cast_skill_to_target(self.custom_skill, target_agent_id=target_spirit_spirit.agent_id)
+        return result
