59a36de48c9d4695be971170929eb47ed9280f98
Alice <alycecil@gmail.com>
2026-03-13 21:51:28 -0400

    glaive

---
+++ Sources/oazix/CustomBehaviors/skills/ritualist/destructive_was_glaive_utility.py
@@ -0,0 +1,62 @@
+from typing import Any, Generator, Callable, override
+
+from Py4GWCoreLib import GLOBAL_CACHE, Agent, Range, Routines, Player
+from Sources.oazix.CustomBehaviors.primitives import constants
+from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
+from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
+from Sources.oazix.CustomBehaviors.primitives.helpers import custom_behavior_helpers
+from Sources.oazix.CustomBehaviors.primitives.helpers.behavior_result import BehaviorResult
+from Sources.oazix.CustomBehaviors.primitives.helpers.targeting_order import TargetingOrder
+from Sources.oazix.CustomBehaviors.primitives.scores.score_per_agent_quantity_definition import \
+    ScorePerAgentQuantityDefinition
+from Sources.oazix.CustomBehaviors.primitives.scores.score_static_definition import ScoreStaticDefinition
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill import CustomSkill
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
+
+
+class DestructiveWasGlaiveUtility(CustomSkillUtilityBase):
+
+    def __init__(self,
+                event_bus: EventBus,
+                current_build: list[CustomSkill],
+                score_definition: ScorePerAgentQuantityDefinition = ScorePerAgentQuantityDefinition(lambda enemy_qte: 90 if enemy_qte >= 4 else 71 if enemy_qte >= 3 else 45 if enemy_qte >= 2 else 15 if enemy_qte >= 1 else 10),
+        ) -> None:
+
+        super().__init__(
+            event_bus=event_bus,
+            skill=CustomSkill("Destructive_Was_Glaive"),
+            in_game_build=current_build,
+            score_definition=score_definition,
+            allowed_states=[BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO]
+        )
+        
+        self.score_definition: ScorePerAgentQuantityDefinition = score_definition
+        self.last_agent_quantity: int = 0
+
+    def _get_targets(self) -> list[custom_behavior_helpers.SortableAgentData]:
+
+        targets = custom_behavior_helpers.Targets.get_all_possible_enemies_ordered_by_priority_raw(
+                    within_range=Range.Spellcast,
+                    condition=lambda agent_id: Agent.IsHexed(agent_id) or Agent.IsConditioned(agent_id),
+                    sort_key=(TargetingOrder.AGENT_QUANTITY_WITHIN_RANGE_DESC, TargetingOrder.HP_ASC),
+                    range_to_count_enemies=GLOBAL_CACHE.Skill.Data.GetAoERange(self.custom_skill.skill_id))
+        return targets
+
+    @override
+    def _evaluate(self, current_state: BehaviorState, previously_attempted_skills: list[CustomSkill]) -> float | None:
+        targets = self._get_targets()
+        if len(targets) == 0:
+            # are we holding anything?
+            is_player_holding_an_item: bool = custom_behavior_helpers.Resources.is_player_holding_an_item()
+
+            if is_player_holding_an_item:
+                return None
+            return 70
+
+        target = targets[0]
+        return self.score_definition.get_score(target.agent_quantity_within_range)
+
+    @override
+    def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
+        result = yield from custom_behavior_helpers.Actions.cast_skill(self.custom_skill)
+        return result
