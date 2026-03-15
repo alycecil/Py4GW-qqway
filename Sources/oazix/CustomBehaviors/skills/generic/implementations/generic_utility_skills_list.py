8736a98f37d6ea0236fa8550bf59b7dc65e1999b
Alice <alycecil@gmail.com>
2026-03-15 14:07:33 -0400

    pve skills without overrides go in the comon section

--- Sources/oazix/CustomBehaviors/skills/generic/implementations/generic_utility_skills_list.py
+++ Sources/oazix/CustomBehaviors/skills/generic/implementations/generic_utility_skills_list.py
@@ -1,17 +1,24 @@
 from Py4GWCoreLib.enums_src.Model_enums import SpiritModelID
+from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
 from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
 from Sources.oazix.CustomBehaviors.primitives.scores.score_static_definition import ScoreStaticDefinition
 from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill import CustomSkill
 from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
 from Sources.oazix.CustomBehaviors.skills.common.signet_of_capture_utility import Signet_of_Capture_Stub
 from Sources.oazix.CustomBehaviors.skills.generic.generic_resurrection_utility import GenericResurrectionUtility
+from Sources.oazix.CustomBehaviors.skills.generic.keep_self_effect_up_utility import KeepSelfEffectUpUtility
 from Sources.oazix.CustomBehaviors.skills.generic.minion_invocation_from_corpse_utility import \
     MinionInvocationFromCorpseUtility
 from Sources.oazix.CustomBehaviors.skills.generic.raw_spirit_utility import RawSpiritUtility
 from Sources.oazix.CustomBehaviors.skills.generic.stub_utility import StubUtility
+from Sources.oazix.CustomBehaviors.skills.mesmer.ether_nightmare_utility import EtherNightmareUtility
 from Sources.oazix.CustomBehaviors.skills.necromancer.signet_of_corruption_utility import SignetOfCorruptionUtility
+from Sources.oazix.CustomBehaviors.skills.paragon.save_yourselves_utility import SaveYourSelfKurzUtility, \
+    SaveYourSelfLuxonUtility
 from Sources.oazix.CustomBehaviors.skills.paragon.spear_of_fury_utility import LuxAttackNearestEnemyHasConditionUtility, \
     KurzAttackNearestEnemyHasConditionUtility
+from Sources.oazix.CustomBehaviors.skills.paragon.watch_yourself_utility import WatchYourselfPowerbatteryUtility
+from Sources.oazix.CustomBehaviors.skills.ritualist.summon_spirit_utility import SummonSpiritUtility
 
 
 class GenericUtilitySkillsList:
@@ -46,7 +53,7 @@ class GenericUtilitySkillsList:
         skills.append(RawSpiritUtility(event_bus=event_bus, skill=CustomSkill("Winter"), current_build=in_game_build, score_definition=ScoreStaticDefinition(20), owned_spirit_model_id=SpiritModelID.WINTER))
 
         skills.append(MinionInvocationFromCorpseUtility(event_bus=event_bus, skill=CustomSkill("Animate_Shambling_Horror"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
-        skills.append( MinionInvocationFromCorpseUtility(event_bus=event_bus, skill=CustomSkill("Animate_Bone_Fiend"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
+        skills.append(MinionInvocationFromCorpseUtility(event_bus=event_bus, skill=CustomSkill("Animate_Bone_Fiend"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
         skills.append(MinionInvocationFromCorpseUtility(event_bus=event_bus, skill=CustomSkill("Animate_Bone_Horror"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
         skills.append(MinionInvocationFromCorpseUtility(event_bus=event_bus, skill=CustomSkill("Animate_Vampiric_Horror"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
         skills.append(MinionInvocationFromCorpseUtility(event_bus=event_bus, skill=CustomSkill("Animate_Bone_Minions"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
@@ -60,13 +67,39 @@ class GenericUtilitySkillsList:
             event_bus=event_bus,
             skill=CustomSkill("Signet_of_Corruption_kurzick"),
             current_build=in_game_build))
-
+        skills.append(EtherNightmareUtility(event_bus=event_bus, current_build=in_game_build, skill=CustomSkill("Ether_Nightmare_luxon")))
+        skills.append(EtherNightmareUtility(event_bus=event_bus, current_build=in_game_build, skill=CustomSkill("Ether_Nightmare_kurzick")))
+        skills.append(SummonSpiritUtility(event_bus=event_bus, skill=CustomSkill("Summon_Spirits_kurzick"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
+        skills.append(SummonSpiritUtility(event_bus=event_bus, skill=CustomSkill("Summon_Spirits_luxon"), current_build=in_game_build, score_definition=ScoreStaticDefinition(25)))
         skills.append(KurzAttackNearestEnemyHasConditionUtility(
             event_bus=event_bus,
             current_build=in_game_build))
         skills.append(LuxAttackNearestEnemyHasConditionUtility(
             event_bus=event_bus,
             current_build=in_game_build))
+        skills.append(SaveYourSelfLuxonUtility(
+            event_bus=event_bus,
+            current_build=in_game_build))
+        skills.append(SaveYourSelfKurzUtility(
+            event_bus=event_bus,
+            current_build=in_game_build))
+        skills.append(KeepSelfEffectUpUtility(event_bus=event_bus, current_build=in_game_build,
+                                              skill=CustomSkill("Selfless_Spirit_luxon"),
+                                              score_definition=ScoreStaticDefinition(20)))
+        skills.append(KeepSelfEffectUpUtility(event_bus=event_bus, current_build=in_game_build,
+                                              skill=CustomSkill("Selfless_Spirit_kurzick"),
+                                              score_definition=ScoreStaticDefinition(20)))
+        skills.append(KeepSelfEffectUpUtility(event_bus=event_bus, skill=CustomSkill("Elemental_Lord_kurzick"), current_build=in_game_build,
+                                              score_definition=ScoreStaticDefinition(25), mana_required_to_cast=10,
+                                              allowed_states=[BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO, BehaviorState.FAR_FROM_AGGRO]))
+        skills.append(KeepSelfEffectUpUtility(event_bus=event_bus, skill=CustomSkill("Elemental_Lord_luxon"), current_build=in_game_build,
+                                              score_definition=ScoreStaticDefinition(25), mana_required_to_cast=10,
+                                              allowed_states=[BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO, BehaviorState.FAR_FROM_AGGRO]))
 
+        # Shouts Section
+        # Really should be limited to imbagon but whatever
+        skills.append(WatchYourselfPowerbatteryUtility(
+            event_bus=event_bus,
+            current_build=in_game_build))
 
         return skills
