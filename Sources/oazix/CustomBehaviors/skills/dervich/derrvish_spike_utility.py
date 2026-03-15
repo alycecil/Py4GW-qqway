94b52b844d7d546bfdab4022b0778060ff6fe6c2
Alice <alycecil@gmail.com>
2026-03-15 14:22:57 -0400

    add ebon escape spike

--- Sources/oazix/CustomBehaviors/skills/dervich/derrvish_spike_utility.py
+++ Sources/oazix/CustomBehaviors/skills/dervich/derrvish_spike_utility.py
@@ -6,6 +6,7 @@ from HeroAI.types import SkillType
 from Py4GWCoreLib import GLOBAL_CACHE, Player, Routines, Range, Agent
 from Py4GWCoreLib.enums_src.GameData_enums import Profession
 from Py4GWCoreLib.py4gwcorelib_src.Utils import Utils
+from Sources.oazix.CustomBehaviors.PersistenceLocator import PersistenceLocator
 from Sources.oazix.CustomBehaviors.primitives import constants
 from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
 from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
@@ -44,7 +45,10 @@ class DervishSpikeUtility(CustomSkillUtilityBase):
         self.aura_of_thorns: CustomSkill = CustomSkill("Aura_of_Thorns")
         self.staggering_force: CustomSkill = CustomSkill("Staggering_Force")
         self.Deaths_Charge: CustomSkill = CustomSkill("Deaths_Charge")
+        self.Ebon_Escape: CustomSkill = CustomSkill("Ebon_Escape")
         self.mana_required_to_cast = mana_required_to_cast
+
+        self.model_id_filter: int = int(PersistenceLocator().skills.read_or_default(self.custom_skill.skill_name, "model_id_filter", "5903"))
 
     def has_dervish_enchantment(self) -> bool:
 
@@ -114,10 +118,68 @@ class DervishSpikeUtility(CustomSkillUtilityBase):
             self.staggering_force.skill_slot)
 
     def is_shadow_step_available(self):
-        return self.Deaths_Charge.skill_slot is not None and  Routines.Checks.Skills.IsSkillSlotReady(self.Deaths_Charge.skill_slot)
+        return self.Deaths_Charge.skill_slot is not None and Routines.Checks.Skills.IsSkillSlotReady(self.Deaths_Charge.skill_slot)
+
+    def is_ebon_escape_available(self):
+        return self.Ebon_Escape.skill_slot is not None and Routines.Checks.Skills.IsSkillSlotReady(self.Ebon_Escape.skill_slot)
 
     def is_aura_of_thorns_available(self):
         return self.aura_of_thorns.skill_slot is not None and Routines.Checks.Skills.IsSkillSlotReady(self.aura_of_thorns.skill_slot)
+
+    def _get_vanguard(self) -> list[custom_behavior_helpers.SortableAgentData] | None:
+
+        vanguard: list[
+            custom_behavior_helpers.SortableAgentData] = custom_behavior_helpers.Targets.get_all_possible_ncs_of_model_ordered_by_priority_raw(
+            model_id=self.model_id_filter,
+            within_range=Range.Spellcast.value * 1.2,
+            sort_key=(TargetingOrder.ENEMIES_QUANTITY_WITHIN_RANGE_DESC, TargetingOrder.HP_ASC),
+            range_to_count_allies=None,
+            range_to_count_enemies=max(GLOBAL_CACHE.Skill.Data.GetAoERange(self.custom_skill.skill_id), Range.Adjacent.value))
+
+        if len(vanguard) == 0: return None
+
+        return vanguard
+
+    def _escape_get_targets(self, target_id) -> list[custom_behavior_helpers.SortableAgentData]:
+
+        allies: list[
+            custom_behavior_helpers.SortableAgentData] = custom_behavior_helpers.Targets.get_all_possible_allies_ordered_by_priority_raw(
+            within_range=Range.Spellcast.value * 1.2,
+            sort_key=(TargetingOrder.ENEMIES_QUANTITY_WITHIN_RANGE_DESC, TargetingOrder.HP_ASC),
+            range_to_count_allies=None,
+            range_to_count_enemies=max(GLOBAL_CACHE.Skill.Data.GetAoERange(self.custom_skill.skill_id), Range.Adjacent.value)
+        )
+
+        vanguard = self._get_vanguard()
+        if vanguard is not None:
+            allies.extend(vanguard)
+
+            # with vanguards added after means prefer player over npc
+            allies = sorted(allies, key=lambda x: -x.enemy_quantity_within_range) # intentional dupe of TargetingOrder.ENEMIES_QUANTITY_WITHIN_RANGE_DESC
+
+        # filter down to just those near target
+        pos = Agent.GetXY(target_id)
+        max_distance = self.range_for_target()
+
+        if len(allies) == 0:
+            if constants.DEBUG: print(f"Failed to find anyone near our target")
+            return allies
+
+        def _distance_filter(agent_id):
+            agent_x, agent_y = Agent.GetXY(agent_id)
+            distance = Utils.Distance((agent_x, agent_y), (pos[0], pos[1]))
+            distance_max_distance = distance <= max_distance
+            # if constants.DEBUG:
+            if distance_max_distance:
+                print(f"{Agent.GetNameByID(agent_id)}, agent_id={agent_id} is {distance} gwinches away")
+            else:
+                print(f"EXCLUDING {Agent.GetNameByID(agent_id)}, agent_id={agent_id} is {distance} gwinches away")
+            return distance_max_distance
+
+        filtered_by_distance_from_target = lambda agent: _distance_filter(agent.agent_id)
+        allies = list(filter(filtered_by_distance_from_target, allies))
+
+        return allies
 
     @override
     def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
@@ -134,35 +196,54 @@ class DervishSpikeUtility(CustomSkillUtilityBase):
                 and Resources.get_player_absolute_energy() > wanted_mana + 10  # todo this better
         ):
             wanted_mana = self.mana_required_to_cast + 10
-            print("using spike with staggering_force")
+            if constants.DEBUG: print("using spike with staggering_force")
             cast_staggering_force = True
 
         if (self.is_aura_of_thorns_available()
               and Resources.get_player_absolute_energy() > wanted_mana + 5  # todo this better
         ):
             wanted_mana = wanted_mana + 5
-            print("using spike with aura_of_thorns")
+            if constants.DEBUG: print("using spike with aura_of_thorns")
             cast_aura_of_thorns = True
 
-        if self.is_shadow_step_available():
+        ebon_escape_available = self.is_ebon_escape_available()
+        shadow_step_available = self.is_shadow_step_available()
+        if shadow_step_available or ebon_escape_available:
             cast_shadow_step = False
 
             if Resources.get_player_absolute_energy() > wanted_mana + 5:
                 # shadowstep?
                 cast_shadow_step = True
-            elif Resources.get_player_absolute_energy() > wanted_mana and cast_staggering_force and cast_aura_of_thorns:
-                cast_staggering_force = False # prefer slower recharge snare if not enough energy
+            elif Resources.get_player_absolute_energy() > wanted_mana - 5 and cast_staggering_force and cast_aura_of_thorns:
+                cast_staggering_force = False  # prefer slower recharge snare if not enough energy
                 cast_shadow_step = True
 
             if cast_shadow_step:
-                # we need a heal or are out of attack range
-                distance = Utils.Distance(Agent.GetXY(target.agent_id), Player.GetXY())
                 player_hp = Agent.GetHealth(Player.GetAgentID())
-                range_to_jump = Range.Nearby.value
-                if Agent.IsCrippled(Player.GetAgentID()):
-                    range_to_jump = Range.Adjacent.value
+                range_to_jump = self.range_for_target()
+
+                distance = Utils.Distance(Agent.GetXY(target.agent_id), Player.GetXY())
                 if distance > range_to_jump or player_hp < 0.6:
-                    yield from custom_behavior_helpers.Actions.cast_skill_to_target(self.Deaths_Charge, target_agent_id=target.agent_id)
+                    has_stepped = False
+                    if ebon_escape_available:
+                        ee_targets = self._escape_get_targets(target.agent_id)
+                        if len(ee_targets) > 0:
+                            ee_target = ee_targets[0]
+                            print(f"using {self.Ebon_Escape.skill_name} to {ee_target} {Agent.GetNameByID(ee_target.agent_id)}")
+                            ee_res = yield from custom_behavior_helpers.Actions.cast_skill_to_target(self.Ebon_Escape, target_agent_id=ee_target.agent_id)
+                            if ee_res == BehaviorResult.ACTION_PERFORMED:
+                                has_stepped = True
+                            else:
+                                if constants.DEBUG:
+                                    print("ee failed, will try to shadow step otherwise")
+                        else:
+                            if constants.DEBUG:
+                                print("Nothing in EE range, will try to fallback to deaths charge")
+
+                    if not has_stepped and shadow_step_available:
+                        # we need a heal or are out of attack range
+                        print(f"using {self.Deaths_Charge.skill_name} to shadowstep")
+                        yield from custom_behavior_helpers.Actions.cast_skill_to_target(self.Deaths_Charge, target_agent_id=target.agent_id)
 
         ping = 150 # todo read from frame
 
@@ -171,17 +252,42 @@ class DervishSpikeUtility(CustomSkillUtilityBase):
             yield from custom_behavior_helpers.Actions.cast_skill(self.staggering_force)
             yield from custom_behavior_helpers.Helpers.wait_for(1000 + ping) # should add ping here
             yield from custom_behavior_helpers.Actions.cast_skill(self.aura_of_thorns)
-
         else:
             if cast_staggering_force:
                 yield from custom_behavior_helpers.Actions.cast_skill(self.staggering_force)
             if cast_aura_of_thorns:
                 yield from custom_behavior_helpers.Actions.cast_skill(self.aura_of_thorns)
 
-
         result = yield from custom_behavior_helpers.Actions.cast_skill_to_target(self.custom_skill, target_agent_id=target.agent_id)
         return result
+
+    def range_for_target(self):
+        range_to_jump = Range.Nearby.value
+        if Agent.IsCrippled(Player.GetAgentID()):
+            range_to_jump = Range.Adjacent.value
+        return range_to_jump
 
     @override
     def customized_debug_ui(self, current_state):
         PyImGui.bullet_text(f"has_dervish_enchantment : {self.has_dervish_enchantment()}")
+
+    @override
+    def has_persistence(self) -> bool:
+        return True
+
+    #TODO UI
+
+    @override
+    def persist_configuration_for_account(self):
+        PersistenceLocator().skills.write_for_account(str(self.custom_skill.skill_name), "model_id_filter", str(self.model_id_filter))
+        print("configuration saved for account")
+
+    @override
+    def persist_configuration_as_global(self):
+        PersistenceLocator().skills.write_global(str(self.custom_skill.skill_name), "model_id_filter", str(self.model_id_filter))
+        print("configuration saved as global")
+
+    @override
+    def delete_persisted_configuration(self):
+        PersistenceLocator().skills.delete(str(self.custom_skill.skill_name), "model_id_filter")
+        print("configuration deleted")
