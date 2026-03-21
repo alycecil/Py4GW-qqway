488283b9bb53d939747cff04a6a69cd00cfdeb1e
Alice <alycecil@gmail.com>
2026-03-21 10:37:58 -0400

    withering aura

---
+++ Sources/oazix/CustomBehaviors/skills/necromancer/Withering_Aura_utility.py
@@ -0,0 +1,137 @@
+from typing import Any, Generator, override
+import PyImGui
+from Py4GWCoreLib.GlobalCache import GLOBAL_CACHE
+from Py4GWCoreLib.enums import Profession, Range
+from Py4GWCoreLib import Agent, Player, Routines, traceback
+from Sources.oazix.CustomBehaviors.PersistenceLocator import PersistenceLocator
+from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
+from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
+from Sources.oazix.CustomBehaviors.primitives.helpers import custom_behavior_helpers
+from Sources.oazix.CustomBehaviors.primitives.helpers.behavior_result import BehaviorResult
+from Sources.oazix.CustomBehaviors.primitives.helpers.lock_key_helper import LockKeyHelper
+from Sources.oazix.CustomBehaviors.primitives.helpers.targeting_order import TargetingOrder
+from Sources.oazix.CustomBehaviors.primitives.parties.custom_behavior_party import CustomBehaviorParty
+from Sources.oazix.CustomBehaviors.primitives.scores.score_static_definition import ScoreStaticDefinition
+from Sources.oazix.CustomBehaviors.primitives.skills.bonds.custom_buff_multiple_target import CustomBuffMultipleTarget
+from Sources.oazix.CustomBehaviors.primitives.skills.bonds.custom_buff_target_per_profession import BuffConfigurationPerProfession
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill import CustomSkill
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
+
+class Withering_Aura_Utility(CustomSkillUtilityBase):
+
+    def __init__(self,
+        event_bus: EventBus,
+        current_build: list[CustomSkill],
+        score_definition: ScoreStaticDefinition = ScoreStaticDefinition(30),
+        mana_required_to_cast: int = 10,
+        allowed_states: list[BehaviorState] = [BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO]
+        ) -> None:
+
+        super().__init__(
+            event_bus=event_bus,
+            skill=CustomSkill("Withering_Aura"),
+            in_game_build=current_build,
+            score_definition=score_definition,
+            mana_required_to_cast=mana_required_to_cast,
+            allowed_states=allowed_states)
+                
+        self.score_definition: ScoreStaticDefinition = score_definition
+
+        data: str | None = PersistenceLocator().skills.read(self.custom_skill.skill_name, "buff_configuration")
+        if data is not None:
+            self.buff_configuration: CustomBuffMultipleTarget = CustomBuffMultipleTarget.instanciate_from_string(self.event_bus, self.custom_skill, data)
+        else:
+            self.buff_configuration: CustomBuffMultipleTarget = CustomBuffMultipleTarget(event_bus, self.custom_skill, buff_configuration_per_profession= BuffConfigurationPerProfession.BUFF_CONFIGURATION_MARTIAL)
+
+        self.should_target_ebon_vanguard_assassin = bool(PersistenceLocator().skills.read_or_default(self.custom_skill.skill_name, "should_target_ebon_vanguard_assassin", "0") == "1")
+        self.ebon_vanguard_assassin_model_id = 5903
+
+    def _get_lock_key(self, agent_id: int) -> str:
+        return f"Withering_Aura_{agent_id}"
+
+    def _get_target(self) -> int | None:
+
+        if self.should_target_ebon_vanguard_assassin:
+            npc_agent_id : int = Routines.Agents.GetNearestAliveAgentByModelID(self.ebon_vanguard_assassin_model_id, Range.Spellcast.value)
+            if npc_agent_id != None and npc_agent_id != 0 and not Agent.IsEnchanted(npc_agent_id):
+                return npc_agent_id
+        
+        # Check if we have a valid target
+        target = custom_behavior_helpers.Targets.get_first_or_default_from_allies_ordered_by_priority(
+                within_range=Range.Spellcast.value * 1.2,
+                condition=lambda agent_id: 
+                    agent_id != Player.GetAgentID() and
+                    self.buff_configuration.get_agent_id_predicate()(agent_id)  and
+                    not CustomBehaviorParty().get_shared_lock_manager().is_lock_taken(self._get_lock_key(agent_id)),
+                sort_key=(TargetingOrder.DISTANCE_DESC, TargetingOrder.CASTER_THEN_MELEE),
+                range_to_count_enemies=None,
+                range_to_count_allies=None)
+
+        if target is not None and target > 0:
+            return target
+
+        # Fallback if no other target
+        npc_agent_id : int = Routines.Agents.GetNearestAliveAgentByModelID(self.ebon_vanguard_assassin_model_id, Range.Spellcast.value)
+        if npc_agent_id != None and npc_agent_id != 0 and not Agent.IsEnchanted(npc_agent_id):
+            return npc_agent_id
+    
+        return target
+
+    @override
+    def _evaluate(self, current_state: BehaviorState, previously_attempted_skills: list[CustomSkill]) -> float | None:
+        target = self._get_target()
+        if target is None: return None
+        return self.score_definition.get_score()
+
+    @override
+    def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
+        target = self._get_target()
+        if target is None: return BehaviorResult.ACTION_SKIPPED
+
+        if not CustomBehaviorParty().get_shared_lock_manager().try_aquire_lock(self._get_lock_key(target), 3):
+            yield
+            return BehaviorResult.ACTION_SKIPPED
+
+        result = yield from custom_behavior_helpers.Actions.cast_skill_to_target(self.custom_skill, target_agent_id=target)
+        return result 
+
+    @override
+    def get_buff_configuration(self) -> CustomBuffMultipleTarget | None:
+        return self.buff_configuration
+    
+    @override
+    def customized_debug_ui(self, current_state: BehaviorState) -> None:
+        ebon_vanguard_assassin_targeting = False
+        try:
+            if self.should_target_ebon_vanguard_assassin:
+                ebon_vanguard_assassin_targeting = True
+
+            from Py4GWCoreLib import PyImGui, ImGui, Color
+
+            self.should_target_ebon_vanguard_assassin = PyImGui.checkbox("should_target_ebon_vanguard_assassin##should_target_ebon_vanguard_assassin",
+                                                                         ebon_vanguard_assassin_targeting)
+        except Exception as e:
+            self.should_target_ebon_vanguard_assassin = True
+            print(f"Failed to render ebon vanguard toggle, defaulting to true {e}:{traceback.format_exc()}")
+
+    @override
+    def has_persistence(self) -> bool:
+        return True
+
+    @override
+    def persist_configuration_for_account(self):
+        PersistenceLocator().skills.write_for_account(str(self.custom_skill.skill_name), "buff_configuration", self.buff_configuration.serialize_to_string())
+        PersistenceLocator().skills.write_for_account(str(self.custom_skill.skill_name), "should_target_ebon_vanguard_assassin", "1" if self.should_target_ebon_vanguard_assassin else "0")
+        print("configuration saved for account")
+
+    @override
+    def persist_configuration_as_global(self):
+        PersistenceLocator().skills.write_global(str(self.custom_skill.skill_name), "buff_configuration", self.buff_configuration.serialize_to_string())
+        PersistenceLocator().skills.write_global(str(self.custom_skill.skill_name), "should_target_ebon_vanguard_assassin", "1" if self.should_target_ebon_vanguard_assassin else "0")
+        print("configuration saved as global")
+
+    @override
+    def delete_persisted_configuration(self):
+        PersistenceLocator().skills.delete(str(self.custom_skill.skill_name), "buff_configuration")
+        PersistenceLocator().skills.delete(str(self.custom_skill.skill_name), "should_target_ebon_vanguard_assassin")
+        print("configuration deleted")
