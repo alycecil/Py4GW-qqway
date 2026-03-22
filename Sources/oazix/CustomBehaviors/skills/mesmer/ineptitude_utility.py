8c32f6a4b8e1555b5dc0bd8b06675df1dc808020
Alice <alycecil@gmail.com>
2026-03-22 14:59:13 -0400

    rewrite to the sort by score

--- Sources/oazix/CustomBehaviors/skills/mesmer/ineptitude_utility.py
+++ Sources/oazix/CustomBehaviors/skills/mesmer/ineptitude_utility.py
@@ -1,6 +1,7 @@
 from typing import Any, Generator, Callable, override
 
 from Py4GWCoreLib import GLOBAL_CACHE, Agent, Range
+from Sources.oazix.CustomBehaviors.primitives import constants
 from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
 from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
 from Sources.oazix.CustomBehaviors.primitives.helpers import custom_behavior_helpers
@@ -20,7 +21,7 @@ class IneptitudeUtility(CustomSkillUtilityBase):
     def __init__(self,
         event_bus: EventBus,
         current_build: list[CustomSkill],
-        score_definition: ScorePerAgentQuantityDefinition = ScorePerAgentQuantityDefinition(lambda enemy_qte: 75 if enemy_qte >= 2 else 40 if enemy_qte >= 2 else 10),
+        score_definition: ScorePerAgentQuantityDefinition = ScorePerAgentQuantityDefinition(lambda enemy_qte: 80 if enemy_qte >= 5 else 55 if enemy_qte >= 3 else 40 if enemy_qte >= 2 else 20),
         mana_required_to_cast: int = 15,
         allowed_states: list[BehaviorState] = [BehaviorState.IN_AGGRO]
         ) -> None:
@@ -38,23 +39,53 @@ class IneptitudeUtility(CustomSkillUtilityBase):
     def _get_lock_key(self, agent_id: int) -> str:
         return f"Ineptitude_{agent_id}"
 
+    def _get_target_score(self, target: custom_behavior_helpers.SortableAgentData) -> float:
+        score_max = 90
+        score_min = 0
+        score_add = 0
+
+        if Agent.IsHexed(target.agent_id):
+            score_max = 20
+        else:
+            score_min = 40
+
+        if Agent.IsMartial(target.agent_id):
+            score_add = 20
+
+        score: int = round(self.score_definition.get_score(target.enemy_quantity_within_range)) + score_add
+
+        return max(min(score_max, score), score_min)
+
     def _get_targets(self) -> list[custom_behavior_helpers.SortableAgentData]:
         """Get martial enemies ordered by cluster size and distance."""
-        return custom_behavior_helpers.Targets.get_all_possible_enemies_ordered_by_priority_raw(
+        by_priority_raw : list[custom_behavior_helpers.SortableAgentData] = custom_behavior_helpers.Targets.get_all_possible_enemies_ordered_by_priority_raw(
                     within_range=Range.Spellcast,
-                    condition=lambda agent_id: Agent.IsMartial(agent_id) and not Agent.IsSpirit(agent_id),
-                    sort_key=(TargetingOrder.AGENT_QUANTITY_WITHIN_RANGE_DESC, TargetingOrder.DISTANCE_ASC),
+                    condition=lambda agent_id: not Agent.IsSpirit(agent_id),
                     range_to_count_enemies=GLOBAL_CACHE.Skill.Data.GetAoERange(self.custom_skill.skill_id))
+
+        by_priority_raw.sort(key=lambda target: (
+            -self._get_target_score(target),
+            -target.enemy_quantity_within_range,
+            target.is_caster,
+        ))
+
+        if constants.DEBUG:
+            print("List of targets")
+            for item in by_priority_raw:
+                print(f"item: {self._get_target_score(item)} : {item}")
+
+        return by_priority_raw
 
     @override
     def _evaluate(self, current_state: BehaviorState, previously_attempted_skills: list[CustomSkill]) -> float | None:
         
         targets = self._get_targets()
         if len(targets) == 0: return None
-        lock_key = self._get_lock_key(targets[0].agent_id)
+        target = targets[0]
+        lock_key = self._get_lock_key(target.agent_id)
         if CustomBehaviorParty().get_shared_lock_manager().is_lock_taken(lock_key): return None
 
-        return self.score_definition.get_score(targets[0].enemy_quantity_within_range)
+        return self._get_target_score(target)
 
     @override
     def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
@@ -64,7 +95,7 @@ class IneptitudeUtility(CustomSkillUtilityBase):
         target = enemies[0]
 
         lock_key = self._get_lock_key(target.agent_id)
-        if CustomBehaviorParty().get_shared_lock_manager().try_aquire_lock(lock_key) == False: 
+        if not CustomBehaviorParty().get_shared_lock_manager().try_aquire_lock(lock_key):
             return BehaviorResult.ACTION_SKIPPED 
 
         try:
