ae78e0c595f5bd74bee7575f88886c01a9b77358
Alice <alycecil@gmail.com>
2026-03-27 23:06:28 -0400

    soul taker way

--- Sources/oazix/CustomBehaviors/skillbars/aura_of_the_lich.py
+++ Sources/oazix/CustomBehaviors/skillbars/aura_of_the_lich.py
@@ -1,5 +1,6 @@
 from typing import override
 
+from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
 from Sources.oazix.CustomBehaviors.primitives.scores.score_per_agent_quantity_definition import \
     ScorePerAgentQuantityDefinition
 from Sources.oazix.CustomBehaviors.primitives.scores.score_static_definition import ScoreStaticDefinition
@@ -11,6 +12,7 @@ from Sources.oazix.CustomBehaviors.skills.common.ebon_vanguard_assassin_support_
 from Sources.oazix.CustomBehaviors.skills.generic.keep_self_effect_up_utility import KeepSelfEffectUpUtility
 from Sources.oazix.CustomBehaviors.skills.generic.preparation_utility import PreparationUtility
 from Sources.oazix.CustomBehaviors.skills.necromancer.aura_of_the_lich_utility import Aura_of_the_Lich_Utility
+from Sources.oazix.CustomBehaviors.skills.necromancer.dark_aura_utility import DarkAuraUtility
 from Sources.oazix.CustomBehaviors.skills.necromancer.necrosis_utility import NecrosisUtility
 
 
@@ -39,21 +41,28 @@ class Aura_of_the_Lich_UtilitySkillBar(CustomBehaviorBaseUtility):
             score_definition=ScorePerAgentQuantityDefinition(lambda enemy_qte: 69),
         )
 
+        self.masochism_utility: CustomSkillUtilityBase = KeepSelfEffectUpUtility(event_bus=self.event_bus, skill=CustomSkill("Masochism"), current_build=in_game_build, score_definition=ScoreStaticDefinition(89))
+
+        self.dark_aura: CustomSkillUtilityBase = DarkAuraUtility(event_bus=self.event_bus, score_definition=ScoreStaticDefinition(80), current_build=in_game_build, mana_required_to_cast=10)
+
         self.serpents_quickness_prep_utility: CustomSkillUtilityBase = PreparationUtility(
             event_bus=self.event_bus,
             prep_skill=CustomSkill("Serpents_Quickness"),
             target_utilities=[self.elite_skill_utility,
                               self.necrosis_utility,
+                              self.masochism_utility
                               ],
             current_build=in_game_build,
-            score_definition=ScoreStaticDefinition(94)
+            score_definition=ScoreStaticDefinition(94),
+            allowed_states=[BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO, BehaviorState.FAR_FROM_AGGRO]
         )
 
         self.dwarven_stability_utility: CustomSkillUtilityBase = KeepSelfEffectUpUtility(
             event_bus=self.event_bus,
             current_build=in_game_build,
             skill=CustomSkill("Dwarven_Stability"),
-            score_definition=ScoreStaticDefinition(95)
+            score_definition=ScoreStaticDefinition(95),
+            allowed_states=[BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO, BehaviorState.FAR_FROM_AGGRO]
         )
 
     @property
@@ -65,6 +74,8 @@ class Aura_of_the_Lich_UtilitySkillBar(CustomBehaviorBaseUtility):
             self.serpents_quickness_prep_utility,
             self.dwarven_stability_utility,
             self.ebon_vanguard_assassin_support,
+            self.masochism_utility,
+            self.dark_aura
         ]
 
     @property
