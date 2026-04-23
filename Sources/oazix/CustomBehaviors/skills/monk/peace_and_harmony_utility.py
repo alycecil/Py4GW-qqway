4d232fd1263c7c4b3b6adfb0867b554d7f619957
Alice <alycecil@gmail.com>
2026-04-23 13:56:35 -0400

    harmony

---
+++ Sources/oazix/CustomBehaviors/skills/monk/peace_and_harmony_utility.py
@@ -0,0 +1,115 @@
+from typing import Any, Generator, override
+
+from Py4GWCoreLib import Range, Agent
+from Sources.oazix.CustomBehaviors.primitives import constants
+from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
+from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
+from Sources.oazix.CustomBehaviors.primitives.helpers import custom_behavior_helpers
+from Sources.oazix.CustomBehaviors.primitives.helpers.behavior_result import BehaviorResult
+from Sources.oazix.CustomBehaviors.primitives.helpers.lock_key_helper import LockKeyHelper
+from Sources.oazix.CustomBehaviors.primitives.helpers.targeting_order import TargetingOrder
+from Sources.oazix.CustomBehaviors.primitives.parties.custom_behavior_party import CustomBehaviorParty
+from Sources.oazix.CustomBehaviors.primitives.scores.score_static_definition import ScoreStaticDefinition
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill import CustomSkill
+from Sources.oazix.CustomBehaviors.primitives.skills.custom_skill_utility_base import CustomSkillUtilityBase
+
+
+class Peace_and_HarmonyUtility(CustomSkillUtilityBase):
+    def __init__(self,
+        event_bus: EventBus,
+        current_build: list[CustomSkill],
+        score_definition: ScoreStaticDefinition = ScoreStaticDefinition(51),
+        mana_required_to_cast: int = 10,
+        allowed_states: list[BehaviorState] = [BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO, BehaviorState.FAR_FROM_AGGRO]
+        ) -> None:
+
+        super().__init__(
+            event_bus=event_bus,
+            skill=CustomSkill("Peace_and_Harmony"),
+            in_game_build=current_build,
+            score_definition=score_definition,
+            mana_required_to_cast=mana_required_to_cast,
+            allowed_states=allowed_states)
+                
+        self.score_definition: ScoreStaticDefinition = score_definition
+
+    def _get_target_score(self, target: custom_behavior_helpers.SortableAgentData) -> float:
+        score_max = 51
+        score_min = 20
+        score_offset = 10
+
+        target_agent_id = target.agent_id
+        if Agent.IsConditioned(target_agent_id) and Agent.IsHexed(target_agent_id):
+            score_offset += 3
+
+        if Agent.IsDegenHexed(target_agent_id):
+            score_offset += 3
+        elif Agent.IsHexed(target_agent_id):
+            score_offset += 2
+        if Agent.IsDeepWounded(target_agent_id):
+            score_offset += 4
+        if Agent.IsBleeding(target_agent_id):
+            score_offset += 2
+        if Agent.IsPoisoned(target_agent_id):
+            score_offset += 2
+
+        lock_key = self._get_lock_key(target_agent_id)
+        if CustomBehaviorParty().get_shared_lock_manager().is_lock_taken(lock_key):
+            score_min = 0
+            score_max = 20
+            score_offset -= 10
+
+        score: int = round(self.score_definition.get_score()) + score_offset
+
+        return max(min(score_max, score), score_min)
+
+    def _get_targets(self) -> list[custom_behavior_helpers.SortableAgentData]:
+        """Get allies that are hexed, ordered by lowest health first."""
+        by_priority_raw: list[custom_behavior_helpers.SortableAgentData] = custom_behavior_helpers.Targets.get_all_possible_allies_ordered_by_priority_raw(
+            within_range=Range.Spellcast.value,
+            condition=lambda agent_id: Agent.IsHexed(agent_id) or Agent.IsConditioned(agent_id)
+        )
+
+        by_priority_raw.sort(key=lambda target: (
+            -self._get_target_score(target),
+            -target.enemy_quantity_within_range,
+            target.is_caster,
+        ))
+
+        if constants.DEBUG:
+            print("List of targets")
+            for item in by_priority_raw:
+                print(f"item: {self._get_target_score(item)} : {item}")
+
+        return by_priority_raw
+
+    def _get_lock_key(self, agent_id: int) -> str:
+        return LockKeyHelper.hex_removal(agent_id)
+
+    @override
+    def _evaluate(self, current_state: BehaviorState, previously_attempted_skills: list[CustomSkill]) -> float | None:
+
+        targets = self._get_targets()
+        if len(targets) == 0: return None
+        target = targets[0]
+        lock_key = self._get_lock_key(target.agent_id)
+        if CustomBehaviorParty().get_shared_lock_manager().is_lock_taken(lock_key): return None
+
+        return self._get_target_score(target)
+
+    @override
+    def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
+
+        targets = self._get_targets()
+        if len(targets) == 0: return BehaviorResult.ACTION_SKIPPED
+        target = targets[0]
+
+        lock_key = self._get_lock_key(target.agent_id)
+        CustomBehaviorParty().get_shared_lock_manager().try_aquire_lock(lock_key, 3) # drop
+
+        try:
+            result = yield from custom_behavior_helpers.Actions.cast_skill_to_target(self.custom_skill, target_agent_id=target.agent_id)
+        finally:
+            CustomBehaviorParty().get_shared_lock_manager().release_lock(lock_key)
+        return result
+
