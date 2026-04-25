9ee8cf081b5aabba98be938ebb761f423b321729
Alice <alycecil@gmail.com>
2026-04-25 18:11:57 -0400

    raw aoe target priority by score

--- Sources/oazix/CustomBehaviors/skills/generic/raw_aoe_attack_utility.py
+++ Sources/oazix/CustomBehaviors/skills/generic/raw_aoe_attack_utility.py
@@ -1,6 +1,8 @@
 from typing import List, Any, Generator, Callable, override
 
-from Py4GWCoreLib import GLOBAL_CACHE, Routines, Range, Agent
+from Py4GWCoreLib import GLOBAL_CACHE, Routines, Range, Agent, Player
+from Py4GWCoreLib.py4gwcorelib_src.Utils import Utils
+from Sources.oazix.CustomBehaviors.primitives import constants
 from Sources.oazix.CustomBehaviors.primitives.behavior_state import BehaviorState
 from Sources.oazix.CustomBehaviors.primitives.bus.event_bus import EventBus
 from Sources.oazix.CustomBehaviors.primitives.helpers import custom_behavior_helpers
@@ -37,6 +39,84 @@ class RawAoeAttackUtility(CustomSkillUtilityBase):
         self.ignore_spirits = ignore_spirits
         self.custom_agent_targeting_predicate: Callable[[int], bool] | None = custom_agent_targeting_predicate
 
+    def _get_target_score(self, target: custom_behavior_helpers.SortableAgentData) -> float:
+        score_max = 55.0
+        score_min = 0.0
+        score_offset = 0.0
+        target_agent_id = target.agent_id
+
+        agent_xy = Agent.GetXY(target_agent_id)
+        player_xy = Player.GetXY()
+        distance = Utils.Distance(agent_xy, player_xy)
+
+        # Called target more important
+        party_target_id = Routines.Party.GetPartyTargetID()
+        if party_target_id == target_agent_id:
+            score_max = 90.0
+            score_offset += 45.0
+            score_min = 40.0
+        else:
+            called_target_xy = Agent.GetXY(party_target_id)
+            distance_called_target = Utils.Distance(agent_xy, called_target_xy)
+            target_range = GLOBAL_CACHE.Skill.Data.GetAoERange(self.custom_skill.skill_id)
+            if target_range is None:
+                target_range = Range.Touch.value
+
+            if distance_called_target < target_range:
+                score_max = 85.0
+                score_offset += 25.0
+                score_min = 20.0
+
+        # is short range skill?
+        short_range = False
+        if GLOBAL_CACHE.Skill.Flags.IsTouchRange(self.custom_skill.skill_id) or GLOBAL_CACHE.Skill.Flags.IsAttack(self.custom_skill.skill_id):
+            short_range = True
+
+        if Agent.IsDeepWounded(target_agent_id):
+            score_offset += 5.0
+
+        if not Agent.IsHexed(target_agent_id):
+            score_offset = -2.0
+        else:
+            score_offset += 1.0
+
+        if Agent.IsCaster(target_agent_id):
+            score_offset += 3.0
+
+        # Health factor
+        health = Agent.GetHealth(target_agent_id)
+
+        if health < .15:
+            score_max = 75.0
+            score_min = 0.0
+            score_offset += 6
+        elif health < .5:
+            score_max = 80.0
+            score_min = 20.0
+            score_offset += 5
+        elif health < .75:
+            score_max = 75.0
+            score_min = 20.0
+            score_offset += 3
+
+        # Distance factor
+        if distance < Range.Touch.value:
+            score_offset += 30 if short_range else 2.2
+        elif distance < Range.Adjacent.value:
+            score_offset += 20 if short_range else 2
+        elif distance < Range.Nearby.value:
+            score_offset += 9 if short_range else 1.5
+        elif distance < Range.Area.value:
+            score_offset += 4 if short_range else 1.1
+        elif distance < Range.Area.value * 2:
+            score_offset += 2 if short_range else 0.5
+        elif distance < Range.Earshot.value:
+            score_offset += 1 if short_range else 0.1
+
+        score: int = round(self.score_definition.get_score(target.enemy_quantity_within_range) + score_offset)
+
+        return max(min(score_max, score), score_min)
+
     def _get_targets(self) -> list[custom_behavior_helpers.SortableAgentData]:
 
         def condition(agent_id: int) -> bool:
@@ -44,18 +124,34 @@ class RawAoeAttackUtility(CustomSkillUtilityBase):
                 return not Agent.IsSpirit(agent_id)
             return True
 
-        return custom_behavior_helpers.Targets.get_all_possible_enemies_ordered_by_priority_raw(
-            condition= lambda agent_id: condition(agent_id) and (self.custom_agent_targeting_predicate is None or self.custom_agent_targeting_predicate(agent_id)),
+        by_priority_raw = custom_behavior_helpers.Targets.get_all_possible_enemies_ordered_by_priority_raw(
+            condition=lambda agent_id: condition(agent_id) and (
+                        self.custom_agent_targeting_predicate is None or self.custom_agent_targeting_predicate(agent_id)
+            ),
             within_range=self.within_range,
             sort_key=(TargetingOrder.AGENT_QUANTITY_WITHIN_RANGE_DESC, TargetingOrder.HP_DESC),
             range_to_count_enemies=GLOBAL_CACHE.Skill.Data.GetAoERange(self.custom_skill.skill_id))
+
+        by_priority_raw.sort(key=lambda target: (
+            -self._get_target_score(target),
+            -target.enemy_quantity_within_range,
+            target.is_caster,
+        ))
+
+        if constants.DEBUG:
+            print("List of targets")
+            for item in by_priority_raw:
+                print(f"item: {self._get_target_score(item)} : {item}")
+
+        return by_priority_raw
 
     @override
     def _evaluate(self, current_state: BehaviorState, previously_attempted_skills: list[CustomSkill]) -> float | None:
         
         targets = self._get_targets()
         if len(targets) == 0: return None
-        return self.score_definition.get_score(targets[0].enemy_quantity_within_range)
+        target = targets[0]
+        return self._get_target_score(target)
 
     @override
     def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
