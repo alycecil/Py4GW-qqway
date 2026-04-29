4baa0e85a77cf0cbc796e1fde448a5a203caf093
Alice <alycecil@gmail.com>
2026-04-28 23:35:30 -0400

    angelic_bond as targeted even if it doesnt need to be

--- Sources/oazix/CustomBehaviors/skills/generic/implementations/generic_utility_skills_list.py
+++ Sources/oazix/CustomBehaviors/skills/generic/implementations/generic_utility_skills_list.py
@@ -180,6 +180,12 @@ class GenericUtilitySkillsList:
         # paragon
         skills.append(ProtectiveShoutUtility(event_bus=event_bus, skill=CustomSkill("Stand_Your_Ground"), current_build=in_game_build, allies_health_less_than_percent=0.9,allies_quantity_required=2,score_definition=ScoreStaticDefinition(88), allowed_states=[BehaviorState.IN_AGGRO]))
         skills.append(ProtectiveShoutTargetedUtility(
+            event_bus=event_bus, skill=CustomSkill("Angelic_Bond"), current_build=in_game_build,
+            allies_health_less_than_percent=0.5,allies_quantity_required=1,
+            score_definition=ScoreStaticDefinition(60),
+            allowed_states=[BehaviorState.IN_AGGRO, BehaviorState.CLOSE_TO_AGGRO, BehaviorState.FAR_FROM_AGGRO, BehaviorState.IDLE])
+        )
+        skills.append(ProtectiveShoutTargetedUtility(
             event_bus=event_bus, skill=CustomSkill("Help_Me"), current_build=in_game_build,
             allies_health_less_than_percent=0.7,allies_quantity_required=1,
             score_definition=ScoreStaticDefinition(88),
