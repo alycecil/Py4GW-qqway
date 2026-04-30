8ead33e6f7ee4ccc7cde9e01541d1dfdace621dc
Alice <alycecil@gmail.com>
2026-04-30 18:49:28 -0400

    adjacent range

--- Sources/oazix/CustomBehaviors/skills/generic/implementations/generic_utility_skills_list.py
+++ Sources/oazix/CustomBehaviors/skills/generic/implementations/generic_utility_skills_list.py
@@ -388,7 +388,7 @@ class GenericUtilitySkillsList:
             mana_required_to_cast=0,
             ignore_spirits=True,
             custom_agent_targeting_predicate=lambda agent_id: Agent.IsCasting(agent_id),
-            within_range=Range.Nearby
+            within_range=Range.Adjacent
         ))
         # Shouts Section
         # Really should be limited to imbagon but whatever
