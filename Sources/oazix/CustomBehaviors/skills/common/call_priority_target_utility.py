7d8a14db331f3b7adaf78e4bc97e1e6dab7fca65
Alice <alycecil@gmail.com>
2026-05-01 10:53:38 -0400

    rework caller

--- Sources/oazix/CustomBehaviors/skills/common/call_priority_target_utility.py
+++ Sources/oazix/CustomBehaviors/skills/common/call_priority_target_utility.py
@@ -4,7 +4,10 @@ from typing import Any, Generator, override
 from Py4GWCoreLib import GLOBAL_CACHE, Routines, Range, Agent, Player
 from Py4GWCoreLib.Py4GWcorelib import ThrottledTimer
 from Py4GWCoreLib.enums_src.GameData_enums import Profession
+from Py4GWCoreLib.enums_src.IO_enums import Key
+from Py4GWCoreLib.py4gwcorelib_src.ActionQueue import ActionQueueManager
 from Py4GWCoreLib.py4gwcorelib_src.Console import ConsoleLog
+from Py4GWCoreLib.py4gwcorelib_src.Keystroke import Keystroke
 from Sources.oazix.CustomBehaviors.primitives import constants
 from Sources.oazix.CustomBehaviors.primitives.helpers import custom_behavior_helpers
 from Sources.oazix.CustomBehaviors.primitives.helpers.behavior_result import BehaviorResult
@@ -43,6 +46,10 @@ class CallPriorityTargetUtility(CustomSkillUtilityBase):
         if self.allowed_states is not None and current_state not in self.allowed_states: return False
         return True
 
+    def __is_there_a_party_target(self) -> bool:
+        party_target_id = Routines.Party.GetPartyTargetID()
+        return party_target_id is not None and party_target_id > 0
+
     def __get_target_agent_id(self) -> int:
         def my_condition(agent_id) -> bool:
 
@@ -56,10 +63,9 @@ class CallPriorityTargetUtility(CustomSkillUtilityBase):
 
             return False
 
-        party_target_id = Routines.Party.GetPartyTargetID()
-        if party_target_id is not None and party_target_id > 0:
+        if self.__is_there_a_party_target():
             if constants.DEBUG:
-                ConsoleLog("call_priority_target_utility", f"Already have a called target {party_target_id}")
+                ConsoleLog("call_priority_target_utility", f"Already have a called target")
             return 0 # Something is already the called target
 
         targets = custom_behavior_helpers.Targets.get_all_possible_enemies_ordered_by_priority_raw(
@@ -84,28 +90,33 @@ class CallPriorityTargetUtility(CustomSkillUtilityBase):
         if not self.throttle_timer.IsExpired():
             return None
 
-        target_agent_id = self.__get_target_agent_id()
-        if target_agent_id == 0: return None
+        if self.__is_there_a_party_target():
+            return None
 
         return self.score_definition.get_score()
+
+    def CallTarget(self, agent_id: int, interact: bool = False) -> bool:
+        if agent_id == 0 or not Agent.IsValid(agent_id):
+            return False
+
+        Player.ChangeTarget(agent_id)
+        if interact:
+            Player.Interact(agent_id, True)
+        ActionQueueManager().AddAction("ACTION", Keystroke.PressAndReleaseCombo, [Key.Ctrl.value, Key.Space.value])
+        return True
 
     @override
     def _execute(self, state: BehaviorState) -> Generator[Any, None, BehaviorResult]:
 
         target_agent_id: int = self.__get_target_agent_id()
-        if target_agent_id == 0: return BehaviorResult.ACTION_SKIPPED
-
-        current_target_agent_id = Player.GetTargetID()
-        if current_target_agent_id != target_agent_id:
-            yield from Routines.Yield.Keybinds.ClearTarget()
-            Player.ChangeTarget(target_agent_id)
-            yield from custom_behavior_helpers.Helpers.wait_for(300)
-            Player.Interact(target_agent_id, True)
-            yield from custom_behavior_helpers.Helpers.wait_for(300)
+        if not self.CallTarget(
+                agent_id=target_agent_id,
+                interact=True
+        ):
+            self.throttle_timer = ThrottledTimer(5000+random.randint(1000, 9999)) # redefine to force jitter in calling
+            yield
+            return BehaviorResult.ACTION_PERFORMED
         else:
-            if not Agent.IsAttacking(Player.GetAgentID()):
-                Player.Interact(target_agent_id, True)
-                yield from custom_behavior_helpers.Helpers.wait_for(300)
-
-        self.throttle_timer = ThrottledTimer(5000+random.randint(1000, 9999)) # redefine to force jitter in calling
-        return BehaviorResult.ACTION_PERFORMED
+            self.throttle_timer = ThrottledTimer(10+random.randint(60, 500))
+            yield
+            return BehaviorResult.ACTION_SKIPPED
